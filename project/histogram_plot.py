a94eff9c5fd0241f5798cd1d8d0658c0b068b147
Oscar Fredriksson <osfr1701@student.miun.se>
2021-08-29 10:44:11 +0000

    added plots

---
+++ project/histogram_plot.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+import sys
+
+file_name = sys.argv[1:][0]
+
+print(file_name)
+
+with open(file_name) as f:
+    data = f.read()
+
+data = data.split('\n')
+
+x = [float(row) for row in data]
+
+print(x)
+
+plt.hist(x)
+
+output_file = file_name.split('.')[0] + ".png"
+
+plt.savefig(output_file)
