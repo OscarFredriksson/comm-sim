f56e287b4566bededa9dcfa223c30b2bcb6d0e9a
Oscar Fredriksson <osfr1701@student.miun.se>
2021-08-31 12:46:01 +0000

    cleaned code

---
+++ project/network.cc
@@ -0,0 +1,62 @@
+#include "network.h"
+
+Network::Network(uint32_t nodes, double simTime) 
+{
+    nodeContainer.Create(nodes);
+    stack.Install(nodeContainer);
+
+    Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
+
+    MobilityHelper help;
+    help.SetMobilityModel("ns3::ConstantPositionMobilityModel");
+    help.InstallAll();
+
+    server_apps.Start(Seconds (1.0));
+    server_apps.Stop(Seconds (simTime));
+}
+
+void Network::addP2PLink(const std::string& dataRate, uint32_t nodeIdA, uint32_t nodeIdB)
+{
+    PointToPointHelper p2pHelper;
+    p2pHelper.SetDeviceAttribute("DataRate", StringValue(dataRate));
+
+    devices.Add(p2pHelper.Install(nodeContainer.Get(nodeIdA), nodeContainer.Get(nodeIdB)));
+
+    std::string ip = "10.1." + std::to_string(ip_it++) + ".0";
+
+    adress.SetBase(ip.c_str(), "255.255.255.0");
+    interfaces = adress.Assign(devices);
+}
+
+Ptr<UdpServer> Network::addUdpServer(uint32_t nodeId) 
+{
+    UdpServerHelper server(port_number);
+    server_apps.Add(server.Install(nodeContainer.Get(nodeId)));
+
+    return server.GetServer();
+}
+
+Ptr<Socket> Network::createConnection(uint32_t nodeId)
+{
+    TypeId tid = TypeId::LookupByName ("ns3::UdpSocketFactory");
+
+    Ptr<Socket> source = Socket::CreateSocket(nodeContainer.Get(nodeId), tid);
+
+    sources.push_back(source);
+
+    return source;
+}
+
+Ptr<Socket> Network::createConnection(uint32_t nodeId, uint32_t destinationAdressId)
+{
+    TypeId tid = TypeId::LookupByName ("ns3::UdpSocketFactory");
+
+    Ptr<Socket> source = Socket::CreateSocket (nodeContainer.Get(nodeId), tid);
+    InetSocketAddress remote = InetSocketAddress (interfaces.GetAddress(destinationAdressId), port_number);
+    source->Connect (remote);
+
+    sources.push_back(source);
+    remotes.push_back(remote);
+
+    return source;
+}
--- project/project.cc
+++ project/project.cc
@@ -1,18 +1,10 @@
-#include "ns3/core-module.h"
-#include "ns3/random-variable-stream.h"
-#include "ns3/core-module.h"
-#include "ns3/network-module.h"
-#include "ns3/csma-module.h"
-#include "ns3/internet-module.h"
-#include "ns3/point-to-point-module.h"
-#include "ns3/applications-module.h"
-#include "ns3/ipv4-global-routing-helper.h"
-#include "ns3/ipv4-address-helper.h"
-#include "ns3/ipv4-global-routing-helper.h"
 #include "ns3/animation-interface.h"
+#include "ns3/flow-monitor-helper.h"
 
 #include <iostream>
 #include <fstream>
+
+#include "network.h"
 
 using namespace ns3;
 
@@ -43,16 +35,16 @@ double exp_dist_from_uniform(double lambda, double u)
 
 static void received_msg (Ptr<Socket> socket1, Ptr<Socket> socket2, Ptr<const Packet> p, const Address &srcAddress , const Address &dstAddress)
 {
-	std::cout << "::::: A packet received at the Server! Time:   " << Simulator::Now ().GetSeconds () << std::endl;
+	//std::cout << "::::: A packet received at the Server! Time:   " << Simulator::Now ().GetSeconds () << std::endl;
 	
 	Ptr<UniformRandomVariable> rand=CreateObject<UniformRandomVariable>();
 	
 	if(rand->GetValue(0.0,1.0)<=0.5){
-		std::cout << "::::: Transmitting from Server to Router   "  << std::endl;
+		//std::cout << "::::: Transmitting from Server to Router   "  << std::endl;
 		socket1->Send (Create<Packet> (p->GetSize ()));
 	}
 	else{
-		std::cout << "::::: Transmitting from GW to Controller   "  << std::endl;
+		//std::cout << "::::: Transmitting from GW to Controller   "  << std::endl;
 		socket2->SendTo(Create<Packet> (p->GetSize ()),0,srcAddress);
 	}
 }
@@ -84,15 +76,13 @@ void SchedulePackets(Ptr<Socket> source, double meanTime, double meanSize) {
    Simulator::ScheduleWithContext (source->GetNode()->GetId(), Seconds (2.0), &GenerateTraffic, source, randomSize, randomTime);
 }
 
-int main (int argc, char *argv[])
+void runPRNGtest() 
 {
   const uint32_t m = 100;
   const uint32_t a = 21;
   const uint32_t c = 1;
   const uint32_t seed = 1;
   LinearCongruentialGenerator lcg(m, a, c, seed);
-
-  //Ptr<UniformRandomVariable> urv = CreateObject<UniformRandomVariable>();
 
   Ptr<ExponentialRandomVariable> erv = CreateObject<ExponentialRandomVariable>();
 
@@ -118,160 +108,118 @@ int main (int argc, char *argv[])
 
   lcg_file.close();
   erv_file.close();
+}
 
-  NodeContainer nodes;
-  nodes.Create(9);
+int main (int argc, char *argv[])
+{
+  //runPRNGtest();
 
-  InternetStackHelper stack;
-  stack.Install(nodes);
+  const double simTime = 10.0; //Seconds
 
-  PointToPointHelper p2pAE;
-  p2pAE.SetDeviceAttribute ("DataRate", StringValue ("5Mbps"));
-  
-  PointToPointHelper p2pEG;
-  p2pEG.SetDeviceAttribute ("DataRate", StringValue ("5Mbps"));
-  
-  PointToPointHelper p2pGS;
-  p2pGS.SetDeviceAttribute ("DataRate", StringValue ("10Mbps"));
-  
-  PointToPointHelper p2pBF;
-  p2pBF.SetDeviceAttribute ("DataRate", StringValue ("5Mbps"));
-  
-  PointToPointHelper p2pFG;
-  p2pFG.SetDeviceAttribute ("DataRate", StringValue ("8Mbps"));
+  Network network(9, simTime);
 
-  PointToPointHelper p2pCF;
-  p2pCF.SetDeviceAttribute ("DataRate", StringValue ("5Mbps"));
+  network.addP2PLink("5Mbps", 0, 4);
+  network.addP2PLink("5Mbps", 1, 5);
+  network.addP2PLink("10Mbps", 2, 5);
+  network.addP2PLink("5Mbps", 3, 6);
+  network.addP2PLink("8Mbps", 4, 6);
+  network.addP2PLink("5Mbps", 6, 7);
+  network.addP2PLink("5Mbps", 5, 6);
+  network.addP2PLink("8Mbps", 6, 8);
 
-  PointToPointHelper p2pDG;
-  p2pDG.SetDeviceAttribute ("DataRate", StringValue ("5Mbps"));
+  // Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
 
-  PointToPointHelper p2pGR;
-  p2pDG.SetDeviceAttribute ("DataRate", StringValue ("8Mbps"));
-
-  Ipv4AddressHelper adress;
-  NetDeviceContainer devices;
-  Ipv4InterfaceContainer interfaces;
-
-  /*
-    A: 0
-    B: 1
-    C: 2
-    D: 3
-    E: 4
-    F: 5
-    G: 6
-    S: 7
-  */
-
-  devices.Add(p2pAE.Install(nodes.Get(0), nodes.Get(4)));
-  adress.SetBase("10.1.1.0", "255.255.255.0");
-  interfaces = adress.Assign(devices);
-
-  devices.Add(p2pBF.Install(nodes.Get(1), nodes.Get(5)));
-  adress.SetBase("10.1.2.0", "255.255.255.0");
-  interfaces = adress.Assign(devices);
-
-  devices.Add(p2pCF.Install(nodes.Get(2), nodes.Get(5)));
-  adress.SetBase("10.1.3.0", "255.255.255.0");
-  interfaces = adress.Assign(devices);
-
-  devices.Add(p2pDG.Install(nodes.Get(3), nodes.Get(6)));
-  adress.SetBase("10.1.4.0", "255.255.255.0");
-  interfaces = adress.Assign(devices);
-
-  devices.Add(p2pEG.Install(nodes.Get(4), nodes.Get(6)));
-  adress.SetBase("10.1.5.0", "255.255.255.0");
-  interfaces = adress.Assign(devices);
-
-  devices.Add(p2pGS.Install(nodes.Get(6), nodes.Get(7)));
-  adress.SetBase("10.1.6.0", "255.255.255.0");
-  interfaces = adress.Assign(devices);
-
-  devices.Add(p2pFG.Install(nodes.Get(5), nodes.Get(6)));
-  adress.SetBase("10.1.7.0", "255.255.255.0");
-  interfaces = adress.Assign(devices);
-
-  devices.Add(p2pGR.Install(nodes.Get(6), nodes.Get(8)));
-  adress.SetBase("10.1.8.0", "255.255.255.0");
-  interfaces = adress.Assign(devices);
-
-
-  Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
+  // MobilityHelper help;
+  // help.SetMobilityModel("ns3::ConstantPositionMobilityModel");
+  // help.InstallAll();
 
   AnimationInterface anim("network.xml");
-  anim.SetConstantPosition(nodes.Get(0), -80.0, -20.0);
-  anim.SetConstantPosition(nodes.Get(1), -80.0, 20.0);
-  anim.SetConstantPosition(nodes.Get(2), -40.0, 60.0);
-  anim.SetConstantPosition(nodes.Get(3), 0.0, 40.0);
-  anim.SetConstantPosition(nodes.Get(4), -40.0, -20.0);
-  anim.SetConstantPosition(nodes.Get(5), -40.0, 20.0);
-  anim.SetConstantPosition(nodes.Get(6), 0.0, 0.0);
-  anim.SetConstantPosition(nodes.Get(7), 0.0, -20.0);
-  anim.SetConstantPosition(nodes.Get(8), 50.0, 0.0);
+  anim.SetConstantPosition(network.nodeContainer.Get(0), -80.0, -20.0);
+  anim.SetConstantPosition(network.nodeContainer.Get(1), -80.0, 20.0);
+  anim.SetConstantPosition(network.nodeContainer.Get(2), -40.0, 60.0);
+  anim.SetConstantPosition(network.nodeContainer.Get(3), 0.0, 40.0);
+  anim.SetConstantPosition(network.nodeContainer.Get(4), -40.0, -20.0);
+  anim.SetConstantPosition(network.nodeContainer.Get(5), -40.0, 20.0);
+  anim.SetConstantPosition(network.nodeContainer.Get(6), 0.0, 0.0);
+  anim.SetConstantPosition(network.nodeContainer.Get(7), 0.0, -20.0);
+  anim.SetConstantPosition(network.nodeContainer.Get(8), 50.0, 0.0);
 
-  NS_LOG_INFO ("Create Applications.");
-	//
-	// Create a UdpServer application on node Server (S).
-	//
-  uint16_t port_number = 9;  
-  ApplicationContainer server_apps;
-  UdpServerHelper serverS (port_number);
-  server_apps.Add(serverS.Install(nodes.Get (7)));
-   
-  Ptr<UdpServer> S1 = serverS.GetServer();
+  network.addUdpServer(0);
+  network.addUdpServer(1);
+  network.addUdpServer(2);
+  network.addUdpServer(3);
 
-  TypeId tid = TypeId::LookupByName ("ns3::UdpSocketFactory");
-   
-   //Transmission Server (S)-> Router (R)
-  Ptr<Socket> sourceRouter = Socket::CreateSocket (nodes.Get(7), tid);
-  InetSocketAddress remoteRouter = InetSocketAddress (interfaces.GetAddress (15), port_number);
-  sourceRouter->Connect (remoteRouter);
-  
-  //Transmission Server (S) -> Client (A or B)
-  Ptr<Socket> sourceClients = Socket::CreateSocket (nodes.Get(7), tid);
-   
-  S1->TraceConnectWithoutContext ("RxWithAddresses", MakeBoundCallback (&received_msg, sourceRouter, sourceClients));
-   
-  server_apps.Start (Seconds (1.0));
-  server_apps.Stop (Seconds (10.0));
-   
-  //
-	// Create a UdpServer application on node A,B,C,D to receive the reply from the server.
-	//
-  UdpServerHelper server (port_number);
-  server_apps.Add(server.Install(nodes.Get (0)));
-  server_apps.Add(server.Install(nodes.Get (1)));
-  server_apps.Add(server.Install(nodes.Get (2)));
-  server_apps.Add(server.Install(nodes.Get (3)));
+  Ptr<UdpServer> server = network.addUdpServer(7);
 
-  Ptr<Socket> sourceA = Socket::CreateSocket (nodes.Get(0), tid);
-  InetSocketAddress remoteA = InetSocketAddress (interfaces.GetAddress(11), port_number);
-  sourceA->Connect (remoteA);
+  Ptr<Socket> routerSource = network.createConnection(7, 15);
+  Ptr<Socket> clientsSource = network.createConnection(7);
 
-  Ptr<Socket> sourceB = Socket::CreateSocket (nodes.Get(1), tid);
-  InetSocketAddress remoteB = InetSocketAddress (interfaces.GetAddress(11), port_number);
-  sourceB->Connect (remoteB);
+  server->TraceConnectWithoutContext("RxWithAddresses", MakeBoundCallback (&received_msg, routerSource, clientsSource));
 
-  Ptr<Socket> sourceC = Socket::CreateSocket (nodes.Get(2), tid);
-  InetSocketAddress remoteC = InetSocketAddress (interfaces.GetAddress(11), port_number);
-  sourceC->Connect (remoteC);
-
-  Ptr<Socket> sourceD = Socket::CreateSocket (nodes.Get(3), tid);
-  InetSocketAddress remoteD = InetSocketAddress (interfaces.GetAddress(11), port_number);
-  sourceD->Connect (remoteD);
+  Ptr<Socket> sourceA = network.createConnection(0, 11);
+  Ptr<Socket> sourceB = network.createConnection(1, 11);
+  Ptr<Socket> sourceC = network.createConnection(2, 11);
+  Ptr<Socket> sourceD = network.createConnection(3, 11);
 
   SchedulePackets(sourceA, 0.002, 100);
   SchedulePackets(sourceB, 0.002, 100);
   SchedulePackets(sourceC, 0.0005, 100);
   SchedulePackets(sourceD, 0.001, 100);
 
-  for(uint32_t i = 0; i < interfaces.GetN(); i++)
-    std::cout << i << ": " << interfaces.GetAddress(i) << "\n";
+  Simulator::Stop(Seconds (simTime));
 
-  Simulator::Stop(Seconds (10));
+  FlowMonitorHelper flowmonHelper;
+  flowmonHelper.InstallAll ();
+  flowmonHelper.SerializeToXmlFile ("network.flowmon", false, false);
+
   Simulator::Run();
   Simulator::Destroy();
 
+  //NS_LOG_INFO ("Create Applications.");
+
+  // Create a UdpServer application on node Server (S).
+  /*uint16_t port_number = 9;  
+  ApplicationContainer server_apps;
+  UdpServerHelper serverS (port_number);
+  server_apps.Add(serverS.Install(network.nodeContainer.Get (7)));
+
+  Ptr<UdpServer> S1 = serverS.GetServer();*/
+
+  //
+  // Create a UdpServer application on node A,B,C,D to receive the reply from the server.
+  //
+  // UdpServerHelper server (port_number);
+  // server_apps.Add(server.Install(network.nodeContainer.Get (0)));
+  // server_apps.Add(server.Install(network.nodeContainer.Get (1)));
+  // server_apps.Add(server.Install(network.nodeContainer.Get (2)));
+  // server_apps.Add(server.Install(network.nodeContainer.Get (3)));
+
+
+  // TypeId tid = TypeId::LookupByName ("ns3::UdpSocketFactory");
+
+  //  //Transmission Server (S)-> Router (R)
+  // Ptr<Socket> sourceRouter = Socket::CreateSocket (network.nodeContainer.Get(7), tid);
+  // InetSocketAddress remoteRouter = InetSocketAddress (network.interfaces.GetAddress (15), network.port_number);
+  // sourceRouter->Connect (remoteRouter);
+
+  // //Transmission Server (S) -> Client (A or B)
+  // Ptr<Socket> sourceClients = Socket::CreateSocket (network.nodeContainer.Get(7), tid);
+
+
+  // Ptr<Socket> sourceA = Socket::CreateSocket (network.nodeContainer.Get(0), tid);
+  // InetSocketAddress remoteA = InetSocketAddress (network.interfaces.GetAddress(11), network.port_number);
+  // sourceA->Connect (remoteA);
+
+  // Ptr<Socket> sourceB = Socket::CreateSocket (network.nodeContainer.Get(1), tid);
+  // InetSocketAddress remoteB = InetSocketAddress (network.interfaces.GetAddress(11), network.port_number);
+  // sourceB->Connect (remoteB);
+
+  // Ptr<Socket> sourceC = Socket::CreateSocket (network.nodeContainer.Get(2), tid);
+  // InetSocketAddress remoteC = InetSocketAddress (network.interfaces.GetAddress(11), network.port_number);
+  // sourceC->Connect (remoteC);
+
+  // Ptr<Socket> sourceD = Socket::CreateSocket (network.nodeContainer.Get(3), tid);
+  // InetSocketAddress remoteD = InetSocketAddress (network.interfaces.GetAddress(11), network.port_number);
+  // sourceD->Connect (remoteD);
 }
 
